27f97b46580b5d6bccb489027c1bbd26726fb77f
ashutoshver <ashutosheng1412@gmail.com>
2022-11-27 19:01:17 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const Selectproduct = document.querySelector("#selectProduct");
 document.querySelector("form").addEventListener("submit", item);
+const container = document.querySelector("#container");
 
 
 function item(event) {
@@ -20,38 +21,31 @@ function item(event) {
     Div.append(Image, btn);
     document.querySelector("#container").append(Div);
 
-    delete button
+    //delete button
     let deleteTask = document.querySelectorAll("button");
     for (let i = 0; i < deleteTask.length; i++) {
       deleteTask[i].onclick = function () {
         this.parentNode.remove();
       }
     }
-
-    //filter buttons
-
-    const filterBtn = document.querySelector("#btn");
-    galleryItems = document.querySelector("#gallery");
-
-    filterBtn.addEventListener("click", (ele) => {
-      if (ele.target.classList.contains("btn1"))
-
-        filterBtn.querySelector(".active").classList.remove("active");
-      ele.target.classList.add("active");
-
-      const filterValue = ele.target.getAttribute("data-filter");
-      galleryItems.forEach((item) => {
-        if (item.classList.contains(filterValue) || filterValue === 'all') {
-          item.classList.remove("hide");
-          item.classList.add("show");
-        }
-        else {
-          item.classList.remove("show");
-          item.classList.add("hide");
-        }
-      })
-    })
-
-
   }
 }
+
+//filter buttons
+const choose = (products, filter) => {
+  container.innerHTML = "";
+  const filterItem = products.filter((item) => filter == 'all' ? true : item.type == filter)
+  console.log(filterItem);
+  filterItem.map((products) => {
+    const newItem = document.createElement("div");
+    newItem.classList.add("item");
+    newItem.classList.add(`${productType.value}`);
+    const productPic = document.createElement("div");
+    productPic.classList.add("Url");
+
+  })
+}
+
+
+
+
